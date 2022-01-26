56c97da6810ef1f5348613388e09b7af321b4bb0
morgane17230 <mgambis@outlook.fr>
2022-01-26 16:31:03 +0100

    deploy fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,8 +30,6 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  target: 'static',
-
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
