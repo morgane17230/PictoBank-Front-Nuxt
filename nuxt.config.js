24f61b4cf9e1775123d3482e824d5eb678152f72
morgane17230 <mgambis@outlook.fr>
2022-03-06 22:50:53 +0100

    defer resources

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,6 +80,7 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
+    defaultAssets: false,
     customVariables: ['~/assets/variables.scss', '~/assets/main.scss'],
     treeShake: true,
     theme: {
