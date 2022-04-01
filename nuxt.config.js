c25dbc0c3a3cb3a1477abcf0a8d0cef386439e31
morgane17230 <mgambis@outlook.fr>
2022-04-01 19:32:35 +0200

    purpose

--- nuxt.config.js
+++ nuxt.config.js
@@ -219,7 +219,7 @@ export default {
       sizes: [64, 120, 144, 152, 192, 384, 512],
       targetDir: 'icons',
       fileName: 'icon.png',
-      purpose: 'maskable'
+      purpose: 'any'
     },
     manifest: {
       name: 'Pikto',
