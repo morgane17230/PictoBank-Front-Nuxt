6fb0d31d6b3923d4685c05cf1704ecfcb737b68e
morgane17230 <mgambis@outlook.fr>
2022-04-01 19:00:53 +0200

    pwa fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
     '@nuxtjs/auth-next',
     '@nuxtjs/dotenv',
     '@nuxt/image',
-
+    '@nuxtjs/pwa',
     'nuxt-purgecss',
     'nuxt-webfontloader',
     '@nuxtjs/robots'
@@ -217,8 +217,9 @@ export default {
     },
     icon: {
       source: '~/static/icon.png',
-      sizes: [64, 120, 144, 152, 192, 384, 512],
+      sizes: '[64, 120, 144, 152, 192, 384, 512]',
       targetDir: 'icons',
+      fileName: 'icon.png',
       purpose: 'maskable'
     },
     manifest: {
