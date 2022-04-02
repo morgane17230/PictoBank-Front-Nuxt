8a70a7c4f56a3f5e1504c9d1bc5b0673c6aa4f8b
morgane17230 <mgambis@outlook.fr>
2022-04-02 18:43:45 +0200

    google verif

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'google-site-verification', content: 's3q-nQ84AjWgDGRCzHUOZp5e5ht3KJfYpOkLF8VL0E0' },
       {
         hid: 'description',
         name: 'description',
